f8d348158744e48c7e944f55c9bc58b9cd523e85
RobinHolden <107262932+RobinHolden@users.noreply.github.com>
2023-06-28 17:22:33 +0200

    Beautify code

--- parser/src/main.c
+++ parser/src/main.c
@@ -8,57 +8,60 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#include "api.h" 
+#include "api.h"
 
-#define false 0 
+#define false 0
 
 int main(int argc,char *argv[])
 {
-	int res,fi;
-	char *p=NULL,*addr;
+	int res, fi;
+	char *p, *addr;
+	struct stat st;
 
-	
-        struct stat st;
+	p = NULL;
 
-	if (argc < 2 ) { printf("Usage: httpparser <file> <search>\nAttention <search> is case sensitive\n");  return 0; }
-	/* ouverture du fichier contenant la requête */ 
-	if ((fi=open(argv[1],O_RDWR)) == -1) {
-                perror("open");
-                return false;
-        }
-        if (fstat(fi, &st) == -1)           /* To obtain file size */
-                return false;
-        if ((addr=mmap(NULL,st.st_size,PROT_WRITE,MAP_PRIVATE, fi, 0)) == NULL )
-                return false;
-
-	// This is a special HACK since identificateur in C can't have character '-'
-
-	if (argc == 3 ) { 
-		p=argv[2]; 	
-		printf("searching for %s\n",p); 
-		while (*p) { 
-			if (*p=='-') { *p='_'; }
-			p++; 
-		}
-		p=argv[2]; 	
+	if (argc < 2 ) {
+		printf("Usage: httpparser <file> <search>\nAttention <search> is case sensitive\n");
+		return 0;
 	}
-	// call parser and get results. 
-	if ((res=parseur(addr,st.st_size))) {
-		_Token *r,*tok; 
-		void *root=NULL; 
-		root=getRootTree();
-		r=searchTree(root,p);
-		tok=r; 
+	/* Ouverture du fichier contenant la requête */
+	if ((fi = open(argv[1], O_RDWR)) == -1) {
+		perror("open");
+		return false;
+    }
+	if (fstat(fi, &st) == -1) /* To obtain file size */
+		return false;
+	if ((addr = mmap(NULL, st.st_size, PROT_WRITE, MAP_PRIVATE, fi, 0)) == NULL)
+		return false;
+	/* This is a special HACK since identificateur in C can't have character '-' */
+	if (argc == 3) { 
+		p = argv[2]; 	
+		printf("Searching for %s\n", p);
+		while (*p) {
+			if (*p == '-') {
+				*p = '_';
+			}
+			p++;
+		}
+		p = argv[2];
+	}
+	/* Call parser and get results. */
+	if ((res = parseur(addr, st.st_size))) {
+		_Token *r, *tok;
+		void *root = NULL;
+		root = getRootTree();
+		r = searchTree(root, p);
+		tok = r;
 		while (tok) {
-			int l; 
-			char *s; 
-			s=getElementValue(tok->node,&l); 
-			printf("FOUND [%.*s]\n",l,s);
-			tok=tok->next; 
+			int l;
+			char *s;
+			s = getElementValue(tok->node, &l);
+			printf("FOUND [%.*s]\n", l, s);
+			tok = tok->next;
 		}
 		purgeElement(&r);
 		purgeTree(root);
 	}
 	close(fi);
-	return(res); 
+	return(res);
 }
